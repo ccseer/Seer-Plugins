744c7d300a0f036f8e32d82228424659de089c74
Uku-Kaarel Jõesaar <uku-kaarel.joesaar@ukj.ee>
2015-12-17 17:31:53 +0200

    Now uses unovonv
    Plugin can work with 32bit versions of Apache OpenOffice 3 and 4 and
    LibreOffice 4 and 5. Libreo_1.cmd file is for soffice.exe in headless
    mode.

--- LibreOffice/libreo.cmd
+++ LibreOffice/libreo.cmd
@@ -1,17 +1,16 @@
 @echo off
-:: /* Uku-Kaarel Jõesaar
-:: ukjoesaar@gmail.com
-:: v1.0 */
-
 for /r %%f in (%1) do (
 Set TempBasename=%%~nf
 )
 
+IF EXIST "C:\Program Files (x86)\OpenOffice.org 3\program\python.exe" SET OfficePyRun=C:\Program Files (x86)\LibreOffice 5\program\python.exe"
+IF EXIST "C:\Program Files (x86)\OpenOffice 4\program\python.exe" SET OfficePyRun=C:\Program Files (x86)\LibreOffice 5\program\python.exe"
+IF EXIST "C:\Program Files (x86)\LibreOffice 4\program\python.exe" SET OfficePyRun=C:\Program Files (x86)\LibreOffice 5\program\python.exe"
+IF EXIST "C:\Program Files (x86)\LibreOffice 5\program\python.exe" SET OfficePyRun=C:\Program Files (x86)\LibreOffice 5\program\python.exe"
+IF EXIST "C:\Program Files (x86)\LibreOfficePortable\App\libreoffice\program\python.exe" SET OfficePyRun="C:\Program Files (x86)\LibreOfficePortable\App\libreoffice\program\python.exe"
 
-Set OfficeRun="C:\Program Files\LibreOffice 5\program\soffice.exe"
+@%OfficePyRun% "C:\Program Files (x86)\Seer\plugins\LibreOffice\unoconv.py" -e PageRange=1,2,3,4,5,6,7,8,9 -f pdf -o "%2.pdf" %1
 
-@%OfficeRun% -headless -convert-to pdf -outdir %TEMP% %1
-@move /Y "%TEMP%\%TempBasename%.pdf" "%2.pdf"
 Set TempBasename=
 
 exit /B 0
