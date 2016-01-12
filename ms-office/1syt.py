5b861b89757d6a175a4bb551e256875b77b2bd15
corey <cc.dreamer@hotmail.com>
2016-01-12 17:59:57 +0800

    0.6.0

--- ms-office/1syt.py
+++ ms-office/1syt.py
@@ -1,76 +1,59 @@
 __author__ = 'corey'
 
+# contributors:
+# ustcltx -- #https://github.com/ustcltx
+
 from win32com.client import DispatchEx
+
+w = ''
 
 
 def doc2pdf(input, output):
+    global w
     w = DispatchEx("Word.Application")
     w.Visible = False
-    try:
-        doc = w.Documents.Open(input, ReadOnly=1)
-        doc.ExportAsFixedFormat(output, 17, False, 1)
-        return 0
-    except:
-        return 1
-    finally:
-        w.Quit()
+    w.DisplayAlerts = False
+    doc = w.Documents.Open(input, ReadOnly=1)
+    doc.ExportAsFixedFormat(output, 17, False, 1)
 
 
 def excel2pdf(input, output):
+    global w
     w = DispatchEx("Excel.Application")
     w.Visible = False
-    try:
-        doc = w.Workbooks.Open(input, ReadOnly=1)
-        doc.ExportAsFixedFormat(0, output)
-        return 0
-    except:
-        return 1
-    finally:
-        w.Quit()
+    w.DisplayAlerts = False
+    doc = w.Workbooks.Open(input, ReadOnly=1)
+    doc.ExportAsFixedFormat(0, output)
 
 
 def ppt2pdf(input, output):
+    global w
     w = DispatchEx("PowerPoint.Application")
-    try:
-        doc = w.Presentations.Open(input, False, False, False)
-        doc.ExportAsFixedFormat(output,
-                                2,
-                                1,
-                                PrintRange=None)
-        return 0
-    except:
-        return 1
-    finally:
-        w.Quit()
+    w.DisplayAlerts = False
+    doc = w.Presentations.Open(input, False, False, False)
+    doc.ExportAsFixedFormat(output, 2, 1, PrintRange=None)
 
 
 def vsd2pdf(input, output):
+    global w
     w = DispatchEx("Visio.Application")
     w.Visible = False
-    try:
-        doc = w.Documents.Open(input)
-        doc.ExportAsFixedFormat(1, output, 1, 0)
-        return 0
-    except Exception as inst:
-        print(type(inst), inst.args, inst)
-        return 1
-    finally:
-        w.Quit()
+    doc = w.Documents.Open(input)
+    doc.ExportAsFixedFormat(1, output, 1, 0)
 
 
 def DispatchFun(path, out):
     p = path.lower()
     if p.endswith('doc') or p.endswith('docx') or p.endswith('rtf'):
-        return doc2pdf(path, out)
+        doc2pdf(path, out)
     elif p.endswith('xls') or p.endswith('xlsx'):
-        return excel2pdf(path, out)
+        excel2pdf(path, out)
     elif p.endswith('ppt') or p.endswith('pptx'):
-        return ppt2pdf(path, out)
+        ppt2pdf(path, out)
     elif p.endswith('vsd') or p.endswith('vsdx'):
-        return vsd2pdf(path, out)
+        vsd2pdf(path, out)
     else:
         print('Unknown format.')
-        return -1
 
 
 def main(path_in, path_out):
@@ -78,22 +61,19 @@ def main(path_in, path_out):
 
     if not os.path.exists(path_in):
         print(path_in + ' not found.')
-        return -1
+        return
 
     if not os.path.isabs(path_in):
         path_in = os.path.abspath(path_in)
     if not os.path.isabs(path_out):
         path_out = os.path.abspath(path_out)
-    try:
-        rc = DispatchFun(path_in, path_out)
-        return rc
-    except:
-        return -1
+
+    DispatchFun(path_in, path_out)
 
 
 def usage(execname):
     print()
-    print("Office2PDF -- part of the Seer app: http://sourceforge.net/projects/ccseer")
+    print("MS_Office2PDF -- part of the Seer app: http://sourceforge.net/projects/ccseer")
     print("----------------------------------------------------------")
     print("Usage: " + execname + " input output")
     print()
@@ -115,6 +95,6 @@ if __name__ == '__main__':
         path_output = sys.argv[2] + '.pdf'
 
     rc = main(path_input, path_output)
-    if rc:
-        sys.exit(rc)
+
+    w.Quit()
     sys.exit(0)
