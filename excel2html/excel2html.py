cef6e6f144090a7514f6c980daa388a7f5ecc540
windows-corey <cc.seer@gmail.com>
2019-05-10 20:13:03 +0800

    excel2html

---
+++ excel2html/excel2html.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+import pyexcel as p
+import sys
+ 
+def convert(path_input, path_output):
+    book = p.get_book(file_name=path_input)
+    book.save_as(path_output, readOnly=True)
+
+
+def usage(execname):
+    print()
+    print("excel2html -- part of the Seer app: http://1218.io")
+    print("----------------------------------------------------------")
+    print("Usage: " + execname + " input output")
+    print()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    print(sys.version)
+    if len(sys.argv) != 3:
+        usage(sys.argv[0])
+
+    path_input = sys.argv[1]
+    path_output = sys.argv[2].lower()
+
+    if path_output.endswith(".handsontable.html") is False:
+        path_output = sys.argv[2] + ".handsontable.html"
+
+    rc = convert(path_input, path_output)
+
+    sys.exit(rc)
