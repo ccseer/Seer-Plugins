627d56b8962bd449cec72ca016eed7d12072b740
coooorey <cc.seer@gmail.com>
2021-07-14 11:38:04 +0800

    work with  *SEER_OMIT_CACHE*"

--- epub/replace.vbs
+++ epub/replace.vbs
@@ -12,6 +12,12 @@ strText = objFile.ReadAll
 objFile.Close
 strNewText = Replace(strText, strOldText, strNewText)
 
-Set objFile = objFSO.OpenTextFile(strFileName, ForWriting)
-objFile.Write strNewText
-objFile.Close
+' Set objFile = objFSO.OpenTextFile(strFileName, ForWriting, true, -2)
+' objFile.Write strNewText
+' objFile.Close
+Set fsT = CreateObject("ADODB.Stream")
+fsT.Type = 2
+fsT.Charset = "utf-8"
+fsT.Open
+fsT.WriteText strNewText
+fsT.SaveToFile strFileName, 2
--- epub/run.bat
+++ epub/run.bat
@@ -1,3 +1,5 @@
+chcp 65001
+
 echo %0 %1 %2
 
 if [%1]==[] (
@@ -9,20 +11,20 @@ if [%2]==[] (
     GOTO  :EOF
 )
 
+@REM c:\aaa\bbb\file.epub
 set target=%1
+@REM c:/aaa/bbb/file.epub
 set target=%target:\=/% 
 
 echo %~dp2, %~nx2, %target%,  "%~dp2%~n2.epub"
 
 xcopy /F /S /Y "./src/*" "%~dp2"
-@REM append _ to the end, otherwise there will be two files with same md5 and different suffix
-copy  /Y %1 "%~dp2%~n2_.epub"
+@REM copy  /Y %1 "%~dp2%~n2_.epub"
 
 set "search=@PLACEHOLDER_FILEPATH@"
 set "replace=%target%"
 
 
-call replace.vbs "%~dp2epub_index.html" "%search%" "%~n2_.epub"
+call replace.vbs "%~dp2epub_index.html" "%search%" %replace%
 
 move /y "%~dp2epub_index.html" "%~dp2%~nx2"
-
